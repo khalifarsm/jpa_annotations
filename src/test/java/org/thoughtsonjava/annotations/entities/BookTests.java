4d9ecb08a1e56ea61346c2682dec5a0947deeaf6
khalifarsm <khalifarsm@outlook.com>
2019-12-06 13:38:26 +0000

    set association test for book

--- src/test/java/org/thoughtsonjava/annotations/entities/BookTests.java
+++ src/test/java/org/thoughtsonjava/annotations/entities/BookTests.java
@@ -3,9 +3,13 @@ package org.thoughtsonjava.annotations.entities;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.thoughtsonjava.annotations.dao.AuthorRepository;
 import org.thoughtsonjava.annotations.dao.BookRepository;
+import org.thoughtsonjava.annotations.dao.PublisherRepository;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 @SpringBootTest
 class BookTests {
@@ -13,11 +17,29 @@ class BookTests {
 	@Autowired
 	BookRepository bookRepository;
 
+	@Autowired
+	PublisherRepository publisherRepository;
+
+	@Autowired
+	AuthorRepository authorRepository;
+
 	@Test
 	void testSave() {
+		Author author = new Author();
+		author.setName("testAuthor");
+		author = authorRepository.save(author);
+
+		Publisher publisher =  new Publisher();
+		publisher.setName("p");
+		publisher = publisherRepository.save(publisher);
+
 		Book book = new Book();
 		book.setTitle("testBook");
 		book.setPublishingDate(LocalDate.now());
+		book.setPublisher(publisher);
+		book.setAuthors(new HashSet<>());
+		book.getAuthors().add(author);
+
 		Long savedBookId = bookRepository.save(book).getId();
 
 		assert(bookRepository.existsById(savedBookId));
@@ -25,6 +47,7 @@ class BookTests {
 
 		assert (b != null);
 		assert (b.getTitle().equals("testBook"));
+		assert (b.getPublisher().getName().equals(publisher.getName()));
 	}
 
 }
